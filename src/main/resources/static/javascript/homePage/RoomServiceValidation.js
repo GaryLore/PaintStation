2d793356e9d1d2c9cc1ae6b3cee1c7b6db3cab66
Gary <garyperez1369@gmail.com>
2026-05-15 18:26:26 -0400

    added functions for readability and reusability

--- src/main/resources/static/javascript/homePage/RoomServiceValidation.js
+++ src/main/resources/static/javascript/homePage/RoomServiceValidation.js
@@ -14,14 +14,12 @@ function enterRoomClientValidation(username, password){
     enter_password_div_error.classList.add("hidden");
 
     if(username.length < 1 || username.length > 10){
-        enter_username_div_error.classList.remove("hidden");
-        enter_username_div_error.textContent = "length must be at least 1 character and at most 10 characters";
+        showError(enter_username_div_error, "length must be at least 1 character and at most 10 characters");
         valid = false;
     }
 
     if(password.length > 64){
-        enter_password_div_error.classList.remove("hidden");
-        enter_password_div_error.textContent = "length must be a max of 64 characters";
+        showError(enter_password_div_error, "length must be a max of 64 characters" )
         valid = false;
     }
 
@@ -36,25 +34,21 @@ function createRoomClientValidation(roomName, ownerName, password){
     create_password_div_error.classList.add("hidden");
 
     if(roomName.length < 5 || roomName.length > 25){
-        create_roomName_div_error.classList.remove("hidden");
-        create_roomName_div_error.textContent = "length must be at least 5 character and at most 25 characters";
+        showError(create_roomName_div_error, "length must be at least 5 character and at most 25 characters");
         valid = false;
     }
     else if(!is_alphanumeric_and_spaces(roomName)){
-        create_roomName_div_error.classList.remove("hidden");
-        create_roomName_div_error.textContent = "Must only contain letters, numbers and spaces";
+        showError(create_roomName_div_error, "Must only contain letters, numbers and spaces");
         valid = false;
     }
 
     if(ownerName.length < 1 || ownerName.length > 10){
-        create_username_div_error.classList.remove("hidden");
-        create_username_div_error.textContent = "length must be at least 1 character and at most 10 characters";
+        showError(create_username_div_error, "length must be at least 1 character and at most 10 characters");
         valid = false;
     }
 
     if(password.length > 64){
-        create_password_div_error.classList.remove("hidden");
-        create_password_div_error.textContent = "length must be a max of 64 characters";
+        showError(create_password_div_error, "length must be a max of 64 characters");
         valid = false;
     }
 
@@ -69,5 +63,12 @@ function is_alphanumeric_and_spaces(name){
 
     return /^[a-zA-Z0-9 ]+$/.test(name);
 }
+
+function showError(div, string) {
+
+    div.classList.remove("hidden");
+    div.textContent = string;
+}
+
 
 export {enterRoomClientValidation, createRoomClientValidation}
